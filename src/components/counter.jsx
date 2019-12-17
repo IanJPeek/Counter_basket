c771b73584ff45c30925e504aae3de4b325d3645
IanJPeek <IanJPeek@gmail.com>
2019-12-17 09:21:52 +0000

    four counters, ready to increment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,35 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { conditionalExpression } from "@babel/types";
 
 class Counter extends Component {
   state = {
-    count: 1,
-    tags: ["tag1", "tag2", "tag3"]
+    count: this.props.value
   };
 
-  render() {
+  handleIncrement =() => {
+    this.setState({count: this.state.count + 1})
+  } 
+
+  render() { 
+    console.log("props", this.props)
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
+        <button onClick={() => {
+    this.handleIncrement()}} 
+        className="btn btn-secondary btn-sm">Increment</button>
+
+        {/* <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+            ))}
+          </ul> */}
 
       </div>
     );
   }
-
+  
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
-
+  
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
   }
-}  
+}
 
- 
 export default Counter;
