566bf6041542ef33ee3f78d9b078c60af74c39b2
IanJPeek <IanJPeek@gmail.com>
2019-12-17 11:19:25 +0000

    Reset button added, functionality/ single source of truth to implement...

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -1,40 +1,53 @@
-   import React, { Component } from 'react';
-   import Counter from "./counter";
-   
-   class Counters extends Component {
-     state = {
-       counters: [
-         { id: 1, value: 0 },
-         { id: 2, value: 0 },
-         {id: 3, value : 0},
-         {id: 4, value : 0}
-       ]
-     };
+import React, { Component } from "react";
+import Counter from "./counter";
 
-     handleDelete = (counterId) => {
-       console.log("Event handler Called", counterId);
-       const counters = this.state.counters.filter(c => c.id !== counterId);
-       this.setState({counters})
-     }
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
 
-     render() {
-       return (
-         <div>
-           {this.state.counters.map(counter => (
-           <Counter key={counter.id} 
-           onDelete={this.handleDelete} 
+  handleReset = () => {
+   const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({counters})
+
+  };
+
+  handleDelete = counterId => {
+    console.log("Event handler Called", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
             counter={counter}
 
-          //  value={counter.value}
-          //    id={counter.id}
-             > 
-           </Counter>
+            //  value={counter.value}
+            //    id={counter.id}
+          ></Counter>
+        ))}
+      </div>
+    );
+  }
+}
 
-           
-           ))}
-         </div>
-       );
-     }
-   }
-   
-   export default Counters;
+export default Counters;
